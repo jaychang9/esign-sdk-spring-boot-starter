1d0753f2d078bb0eb91eb434b06e37487811e25c
jaychang <jaychang1987@gmail.com>
2024-09-04 14:07:27 +0800

    feat: add ESignProperties.java

---
+++ src/main/java/com/zcckj/esign/config/properties/ESignProperties.java
@@ -0,0 +1,22 @@
+package com.zcckj.esign.config.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+/**
+ * e签宝配置
+ *
+ * @author zhangjie
+ * @date 2024-09-04
+ */
+@Data
+@ConfigurationProperties("esign")
+public class ESignProperties {
+	/** 接口基础地址*/
+    private String baseUrl;
+	/** 应用ID*/
+    private String appId;
+	/** 应用密钥*/
+    private String appSecret;
+}
