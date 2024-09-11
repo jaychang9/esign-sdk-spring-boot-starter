f2cb81a724dca9483b33be6c1aa7eb11add9c0ce
jaychang <jaychang1987@gmail.com>
2024-09-11 08:49:58 +0800

    feat: 发起签署经办人字段修改

--- src/main/java/com/zcckj/esign/dto/TransactorInfo.java
+++ src/main/java/com/zcckj/esign/dto/TransactorInfo.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 
 /**
  * 企业/机构经办人信息
+ *
  * @author zhangjie
  */
 @Data
@@ -23,7 +24,15 @@ public class TransactorInfo implements Serializable {
     private String psnAccount;
 
     /**
+     * 经办人账号ID（指定orgId时，该参数为必传项）
+     * <p>
+     * 【注】用户在e签宝注册实名后才有账号ID，账号ID获取方式请使用【查询个人认证信息】接口通过个人账号标识（手机号或邮箱）/个人用户的证件号进行查询
+     * https://open.esign.cn/doc/opendoc/auth3/vssvtu
+     */
+    private String psnId;
+
+    /**
      * 经办人身份信息
      */
-    private PersonInfo psnInfo;
+    private PersonIdInfo psnInfo;
 }
